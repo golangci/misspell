f33d7fdd6dd3f230fbd45f2c6295dc69895c1313
Fernandez Ludovic <ldez@users.noreply.github.com>
2024-04-19 02:19:18 +0200

    fix: improve host name detection

--- notwords_test.go
+++ notwords_test.go
@@ -14,8 +14,20 @@ func TestNotWords(t *testing.T) {
 		{word: "[/foo/bar] abc", want: "[        ] abc"},
 		{word: "/", want: "/"},
 		{word: "x nickg@client9.xxx y", want: "x                   y"},
+		{word: "x fqdn.example.org. y", want: "x                 . y"},
 		{word: "x infinitie.net y", want: "x               y"},
-		{word: "(s.svc.GetObject(", want: "(               ("},
+		{word: "x infinitie.net ", want: "x               "},
+		{word: "x infinitie.net", want: "x              "},
+		{word: "x foo.example.com y", want: "x                 y"},
+		{word: "x foo.example.com ", want: "x                 "},
+		{word: "x foo.example.com", want: "x                "},
+		{word: "foo.example.com y", want: "                y"},
+		{word: "foo.example.com", want: "               "},
+		{word: "(s.svc.GetObject(", want: "(s.svc.GetObject("},
+		{word: "defer file.Close()", want: "defer file.Close()"},
+		{word: "defer file.c()", want: "defer file.c()"},
+		{word: "defer file.cl()", want: "defer        ()"},       // false negative
+		{word: "defer file.close()", want: "defer           ()"}, // false negative
 		{word: "\\nto", want: "  to"},
 	}
 
